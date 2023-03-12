099ed07bc233bde23d5ebb5f9f7786dd946b8cd3
kouyama0201 <kami19900201@gmail.com>
2023-03-12 16:27:37 +0900

    55. データの検索機能を実装する

---
+++ src/app/member-search/member-search.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { Member } from '../member';
+import { MemberService } from '../member.service';
+
+@Component({
+  selector: 'app-member-search',
+  templateUrl: './member-search.component.html',
+  styleUrls: ['./member-search.component.css']
+})
+export class MemberSearchComponent {
+  members$!: Observable<Member[] | any>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private memberService: MemberService) { }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.members$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.memberService.searchMembers(term))
+    )
+  }
+}
